f187db76cd35513da0352e940b660baa4c34f27c
raya1ts <442201815@student.ksu.edu.sa>
2023-04-12 11:59:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/swelab4/StudentMod.java
@@ -0,0 +1,45 @@
+package com.example.swelab4;
+
+public class StudentMod {
+
+    int id ;String name; int age;
+
+    public StudentMod(int id,String name,int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentModel : " +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age ;
+    }
+}
